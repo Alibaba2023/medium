f96f4d9a6f99908f1a6e2da6d78cfc65a41ff937
Ali Baba <ali7090370@gmail.com>
2024-10-17 11:09:41 +0430

    bug fixed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
-            className="w-4 object-contain cursor-pointer"
+            className="w-44 object-contain cursor-pointer"
             src="https://links.papareact.com/yvf"
             alt="The medium icon"
           />
@@ -14,10 +14,17 @@ function Header() {
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold">ali</h1>
       <Header />
     </div>
   );
